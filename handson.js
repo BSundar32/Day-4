3252ae895cd4e0c8d7017d61871976014b6681b5
BSundar32 <sundarb3898@gmail.com>
2025-09-08 19:38:04 +0530

    Updated

--- handson.js
+++ handson.js
@@ -1,8 +1,47 @@
 //
 
 //Print Odd number in an array using normal function
+// var oddNumbers = [];
+// function printOddNumbers(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] % 2 !== 0) {
+//             oddNumbers.push(arr[i]);
+//         }
+//     }
+//     return oddNumbers;
+// }
+
+// console.log(printOddNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+// //print odd number in an array using anonymous function
+// var oddNumbers1 = [];
+// const printOddNumbers1 = function (arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] % 2 !== 0) {
+//             oddNumbers1.push(arr[i]);
+//         }
+//     }
+//     return oddNumbers1;
+// }
+
+// console.log(printOddNumbers1([11, 12, 13, 14, 15, 16, 17, 18, 19]));
+
+//print odd number in an array using IIFE
+// var oddNumbers = [];
+
+// (function (arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] % 2 !== 0) {
+//             oddNumbers.push(arr[i]);
+//         }
+//     }
+// })([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+// console.log(oddNumbers);
+
+// //Print Odd number in an array using arrow function
 var oddNumbers = [];
-function printOddNumbers(arr) {
+const printOddNumbers = (arr) => {
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] % 2 !== 0) {
             oddNumbers.push(arr[i]);
@@ -12,16 +51,3 @@ function printOddNumbers(arr) {
 }
 
 console.log(printOddNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9]));
-
-//print odd number in an array using anonymous function
-var oddNumbers1 = [];
-const printOddNumbers1 = function (arr) {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] % 2 !== 0) {
-            oddNumbers1.push(arr[i]);
-        }
-    }
-    return oddNumbers1;
-}
-
-console.log(printOddNumbers1([11, 12, 13, 14, 15, 16, 17, 18, 19]));
